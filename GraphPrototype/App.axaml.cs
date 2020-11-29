4327565fcaf6f8018505f383487143405d9209e3
Stephen Edmonds <github@stephenedmonds.co.uk>
2020-11-29 22:34:53 +0000

    Drawing a graph

--- GraphPrototype/App.axaml.cs
+++ GraphPrototype/App.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using ScottPlot.Avalonia;
 
 namespace GraphPrototype
 {
--- GraphPrototype/MainWindow.axaml.cs
+++ GraphPrototype/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using ScottPlot.Avalonia;
 
 namespace GraphPrototype
 {
@@ -17,6 +18,13 @@ namespace GraphPrototype
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            var avaplot1 = this.Find<AvaPlot>("avaPlot1");
+
+            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+            avaplot1.plt.PlotScatter(dataX, dataY);
+            avaplot1.Render();
         }
     }
 }
